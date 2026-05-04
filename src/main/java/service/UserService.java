fc146f02395b2f56936916d144a7fd9b7b5e97cf
warning1210 <jieon6725@gmail.com>
2026-05-04 19:55:30 +0900

    add password encryption

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,6 +2,7 @@ package service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import model.User;
 import repository.UserDAO;
@@ -10,14 +11,22 @@ import repository.UserDAO;
 public class UserService {
 
     @Autowired
-    private repository.UserDAO dao;
+    private UserDAO dao;
 
     public void join(User user){
-    	System.out.println("Service 실행됨");
+        System.out.println("Service 실행됨");
+
+        String enc = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        user.setPassword(enc);
+
         dao.insert(user);
     }
 
     public User login(User user){
+
+        String enc = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        user.setPassword(enc);
+
         return dao.login(user);
     }
 }
